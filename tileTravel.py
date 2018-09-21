bedaa660ef82a5b691b65571d6d362bf4255518e
María Rún <Mariarunkarls@MacBook-Air.local>
2018-09-21 12:08:30 +0000

    https://github.com/mariak18/TileTravel

--- tileTravel.py
+++ tileTravel.py
@@ -3,64 +3,88 @@
 # ef inputið er ekki gilt prentast út að það sé ekki gilt og gefur þér vagmöguleikana hvert þú getur farið og leyfir að reyna aftur 
 # Forritið leyfir að setja inn input eins oft áður en það kemur að sigur kassanaum (3.1) og prentar út "Victory!"
 
+#1.  Which implementation was easier and why?
+    #Fyrri kóðin var einfaldari því að við höfum verið að forrita þannig frá byrjuum erum ný byrjuð að læra um föll. 
+#2. Which implementation is more readable and why?
+    #Seinni kóðinn er læsilegri því við erum að nota föll og þurfum ekki að setja alltaf það sama aftur og aftur. 
+#3. Which problems in the first implementations were you able to solve with the latter implementation?
+    #Kóðinn yfir allt er læsilegri og aðalkóðinn er styttri. 
 
-start_x = 1 
-start_y = 1 
+def x_y(x,y):
+    '''Tekur inn gildið x og y '''
+    return x, y 
 
-while start_x != 3 or start_y != 1 :
-    if start_x == 1:   
+def direction_form_input():
+    '''Tekur inn átt frá notenda'''
+    a = input("Direction: ")
+    a = a.lower()
+    return a 
+
+
+def find_valid_dir(a):
+    if a == 1:   
         if start_y == 1 :
-            valid_di = "Nn"
+            valid_di = "n"
             print("You can travel: (N)orth.")
         elif start_y == 2:
-            valid_di = "NnEeSs"
+            valid_di = "nes"
             print("You can travel: (N)orth or (E)ast or (S)outh.")
         elif start_y == 3 :
-            valid_di = "EeSs"
+            valid_di = "es"
             print("You can travel: (E)ast or (S)outh.")
-    elif start_x == 2 :
+    elif a == 2 :
         if start_y == 1 :
-            valid_di = "Nn"
+            valid_di = "n"
             print("You can travel: (N)orth.")    
         elif start_y == 2 :
-            valid_di = "WwSs"
+            valid_di = "ws"
             print("You can travel: (S)outh or (W)est.")
         elif start_y == 3 :
-            valid_di = "EeWw"
+            valid_di = "ew"
             print("You can travel: (E)ast or (W)est.")
-    elif start_x == 3 :
+    elif a == 3 :
         if start_y == 1 :
-            valid_di = "Nn"
+            valid_di = "n"
             print("You can travel: (N)orth.")
         elif start_y == 2 :
-            valid_di = "NnSs"
+            valid_di = "ns"
             print("You can travel: (N)orth or (S)outh.")
         elif start_y == 3 :
-            valid_di = "WwSs"
+            valid_di = "ws"
             print("You can travel: (S)outh or (W)est.")
-    
-    direction = input("Direction: ")
+    return valid_di
 
-    if direction in valid_di :
-        if direction in "Nn":
-            start_y += 1
-        elif direction in "Ee":
-            start_x += 1
-        elif direction in "Ss":
-            start_y -= 1 
-        elif direction in "Ww":
-            start_x -= 1 
+
+def direction_in(d, x, y) :
+    ''' Uppfæra x og y '''
+    if d == "n":
+        y += 1
+    elif d == "e":
+        x += 1
+    elif d == "s":
+        y -= 1 
+    elif d == "w":
+        x -= 1 
+    return x, y
+
+def valid_input(d, v, x, y):
+    if d in v:
+        x, y = direction_in(direction, start_x, start_y)
     else : 
         print("Not a valid direction!")
-        direction = input("Direction: ")
-        if direction in valid_di :
-            if direction in "Nn":
-                start_y += 1
-            elif direction in "Ee":
-                start_x += 1
-            elif direction in "Ss":
-                start_y -= 1 
-            elif direction in "Ww":
-                start_x -= 1 
+        d = direction_form_input()
+        if d in v :
+            x, y = direction_in(d, start_x, start_y)
+    return x, y
+
+
+start_x, start_y = x_y(1,1)
+
+while start_x != 3 or start_y != 1 :
+    valid_di = find_valid_dir(start_x)
+   
+    direction = direction_form_input()
+
+    start_x, start_y = valid_input(direction, valid_di, start_x, start_y)
 else : 
     print("Victory!")
